7bdbd6a7127cd3b1b558be2de7ce3b6fa4328f98
Moyashi911 <sabanomisoni911@gmail.com>
2024-03-16 23:25:42 +0900

    A

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
+import pandas as pd
 uploaded_file = st.file_uploader("アクセスログをアップロードしてください。")
-x = 10
-'x: ', x 
+if uploaded_file is not None:
+    df = pd.read_csv(
+        uploaded_file,
+        sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
+        engine='python',
+        na_values='-',
+        header=None)
+st.markdown('### アクセスログ（先頭5件）')
+st.write(df.head(5))
